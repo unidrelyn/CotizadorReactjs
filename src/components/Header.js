4c57691fef24f4c251269340847c775bf5c110fb
unknown <unidrelyn@gmail.com>
2021-08-24 15:17:41 +0200

    start

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const ContenerdorHeader = styled.header`
+	background-color: #26c6da;
+	padding: 10px;
+	font-weight: bold;
+	color: #ffffff;
+`;
+
+const TextoHeader = styled.h1`
+	font-size: 2rem;
+	margin: 0;
+	font-family: "Slabo 27px", serif;
+	text-align: center;
+`;
+
+const Header = ({ titulo }) => {
+	return (
+		<ContenerdorHeader>
+			<TextoHeader>{titulo}</TextoHeader>
+		</ContenerdorHeader>
+	);
+};
+
+export default Header;
